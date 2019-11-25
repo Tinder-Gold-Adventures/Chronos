6c5db66194658c4de1b29ca9b53f20930dbac251
Jofairden <jofairden@gmail.com>
2019-11-25 19:25:13 +0100

    Update MqttTopicBuilder

--- src/main/kotlin/tinder/gold/adventures/chronos/model/mqtt/builder/MqttTopicBuilder.kt
+++ src/main/kotlin/tinder/gold/adventures/chronos/model/mqtt/builder/MqttTopicBuilder.kt
@@ -4,7 +4,7 @@ import tinder.gold.adventures.chronos.model.traffic.control.ITrafficControl
 
 object MqttTopicBuilder {
 
-    //<team_id>/<lane_type>/<group_id>/<subgroup_id>/<component_type>/<component_id>
+    //<team_id>/<lane_type>/<group_id>/<component_type>/<component_id>
 
     private const val TEAM_ID = 24
 
@@ -27,21 +27,13 @@ object MqttTopicBuilder {
     enum class ComponentType {
         TRAFFIC_LIGHT,
         WARNING_LIGHT,
+        BOAT_LIGHT,
         SENSOR,
-        BARRIER
+        BARRIER,
+        DECK
     }
 
-    fun getTopicString(subject: MqttTopicBuilderSubject, control: ITrafficControl): String {
-        val subgroupId = try {
-            control.overrideSubgroup ?: subject.getSubgroupId(control.directionTo)
-        } catch (_: Exception) {
-            -1
-        }
-
-        val hasSubgroup = subgroupId != -1
-        return if (hasSubgroup)
-            "${TEAM_ID}/${subject.getLaneType()}/${subject.getGroupId(control)}/${subgroupId}/${subject.getComponentType()}/${control.componentId}"
-        else "${TEAM_ID}/${subject.getLaneType()}/${subject.getGroupId(control)}/${subject.getComponentType()}/${control.componentId}"
-    }
+    fun getTopicString(subject: MqttTopicBuilderSubject, control: ITrafficControl): String
+            = "${TEAM_ID}/${subject.getLaneType()}/${subject.getGroupId(control)}/${subject.getComponentType()}/${control.componentId}"
 
 }
